802ca1def5048d60606fea7362e659fdcbe5de64
Enzo Wakida <enzosapper@gmail.com>
2026-06-24 12:31:14 -0300

    Update atividade.bat

--- Semestre 1/aulabat/atividade.bat
+++ Semestre 1/aulabat/atividade.bat
@@ -1,1 +1,7 @@
+echo off
 
+msg * "Por questoes de seguranca, recomendamos mudar todas as suas senhas. Com isso, voce pode evitar invasoes e roubo de dados importantes."
+
+start control userpasswords
+
+pause
